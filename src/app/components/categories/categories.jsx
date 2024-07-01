0a65ad04bc22fdd913637b500fdda278636aaf39
Andriy_Step <andystep2008@gmail.com>
2024-07-01 19:46:08 +0300

    fixing deploy

--- src/app/components/categories/categories.jsx
+++ src/app/components/categories/categories.jsx
@@ -3,27 +3,21 @@ import React from "react";
 import Image from "next/image";
 import './categories.scss'
 
-import Face from './katalog/face.png'
-import Eyes from './katalog/eyes.png'
-import Body from './katalog/body.png'
-import Hair from './katalog/hair.png'
-import Sun from './katalog/sun.png'
-
 const Categories = () => {
     return(
         <div className="categories">
             <h2>Категорії</h2>
             <div className="rows">
                 <div className="row typeOne">
-                    <div className="img one face" style={{backgroundImage: `url(${Face})`}}></div>
+                    <div className="img one face" style={{backgroundImage: `url(/katalog/face.png)`}}></div>
                 </div>
                 <div className="row typeTwo">
-                    <div className="img two eyes" style={{backgroundImage:  `url(${Eyes})`}}></div>
-                    <div className="img two body" style={{backgroundImage:  `url(${Body})`}}></div>
+                    <div className="img two eyes" style={{backgroundImage:  `url(/katalog/eyes.png)`}}></div>
+                    <div className="img two body" style={{backgroundImage:  `url(/katalog/body.png)`}}></div>
                 </div>
                 <div className="row typeTwo">
-                    <div className="img two hair" style={{backgroundImage:  `url(${Hair})`}}></div>
-                    <div className="img two sun" style={{backgroundImage:  `url(${Sun})`}}></div>
+                    <div className="img two hair" style={{backgroundImage:  `url(/katalog/hair.png)`}}></div>
+                    <div className="img two sun" style={{backgroundImage:  `url(/katalog/sun.png)`}}></div>
                 </div>
             </div>
         </div>
