b857e0bc1b70cea5d65227d761c5a82caceeb075
Andriy_Step <andystep2008@gmail.com>
2024-07-03 19:59:15 +0300

    fixing katalog 8

--- src/app/components/categories/categories.jsx
+++ src/app/components/categories/categories.jsx
@@ -4,50 +4,20 @@ import Image from "next/image";
 import './categories.scss'
 
 const Categories = () => {
-    return (
+    return(
         <div className="categories">
             <h2>Категорії</h2>
             <div className="rows">
                 <div className="row typeOne">
-                    <Image 
-                        className="img one face"
-                        src="https://images-orcin-iota.vercel.app/face.png"
-                        alt="Face"
-                        layout="fill"
-                        objectFit="cover"
-                    />
+                    <div className="img one face"></div>
                 </div>
                 <div className="row typeTwo">
-                    <Image 
-                        className="img two eyes"
-                        src="https://images-orcin-iota.vercel.app/eyes.png"
-                        alt="Eyes"
-                        layout="fill"
-                        objectFit="cover"
-                    />
-                    <Image 
-                        className="img two body"
-                        src="https://images-orcin-iota.vercel.app/body.png"
-                        alt="Body"
-                        layout="fill"
-                        objectFit="cover"
-                    />
+                    <div className="img two eyes"></div>
+                    <div className="img two body"></div>
                 </div>
                 <div className="row typeTwo">
-                    <Image 
-                        className="img two hair"
-                        src="https://images-orcin-iota.vercel.app/hair.png"
-                        alt="Hair"
-                        layout="fill"
-                        objectFit="cover"
-                    />
-                    <Image 
-                        className="img two sun"
-                        src="https://images-orcin-iota.vercel.app/sun.png"
-                        alt="Sun"
-                        layout="fill"
-                        objectFit="cover"
-                    />
+                    <div className="img two hair"></div>
+                    <div className="img two sun"></div>
                 </div>
             </div>
         </div>
