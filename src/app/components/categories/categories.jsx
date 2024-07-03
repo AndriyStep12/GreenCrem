356da696011f843d020916e6db43008ef102f328
Andriy_Step <andystep2008@gmail.com>
2024-07-03 19:43:00 +0300

    fixing katalog 5

--- src/app/components/categories/categories.jsx
+++ src/app/components/categories/categories.jsx
@@ -4,50 +4,20 @@ import Image from "next/image";
 import './categories.scss'
 
 const Categories = () => {
-    return (
+    return(
         <div className="categories">
             <h2>Категорії</h2>
             <div className="rows">
                 <div className="row typeOne">
-                    <Image 
-                        className="img one face"
-                        src="/face.png"
-                        alt="Face"
-                        layout="fill"
-                        objectFit="cover"
-                    />
+                    <div className="img one face"></div>
                 </div>
                 <div className="row typeTwo">
-                    <Image 
-                        className="img two eyes"
-                        src="/eyes.png"
-                        alt="Eyes"
-                        layout="fill"
-                        objectFit="cover"
-                    />
-                    <Image 
-                        className="img two body"
-                        src="/body.png"
-                        alt="Body"
-                        layout="fill"
-                        objectFit="cover"
-                    />
+                    <div className="img two eyes"></div>
+                    <div className="img two body"></div>
                 </div>
                 <div className="row typeTwo">
-                    <Image 
-                        className="img two hair"
-                        src="/hair.png"
-                        alt="Hair"
-                        layout="fill"
-                        objectFit="cover"
-                    />
-                    <Image 
-                        className="img two sun"
-                        src="/katalog/sun.png"
-                        alt="Sun"
-                        layout="fill"
-                        objectFit="cover"
-                    />
+                    <div className="img two hair"></div>
+                    <div className="img two sun"></div>
                 </div>
             </div>
         </div>
