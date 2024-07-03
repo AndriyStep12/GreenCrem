fed64ea780225f6da919d87d0b276a3ece4850af
Andriy_Step <andystep2008@gmail.com>
2024-07-03 20:14:00 +0300

    fixing

--- src/app/components/categories/categories.jsx
+++ src/app/components/categories/categories.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
-import Image from "next/image";
-import './categories.scss'
+import './categories.scss';
 import Link from "next/link";
 
 const Categories = () => {
@@ -10,25 +9,35 @@ const Categories = () => {
             <h2>Категорії</h2>
             <div className="rows">
                 <div className="row typeOne">
-                    {/* <Link href="/products/face"> */}
-                        <div className="img one face"></div>
-                    {/* </Link> */}
+                    <Link href="/products/face" passHref>
+                        <a>
+                            <div className="img one face"></div>
+                        </a>
+                    </Link>
                 </div>
                 <div className="row typeTwo">
-                    {/* <Link href="/products/eyes"> */}
-                        <div className="img two eyes"></div>
-                    {/* </Link> */}
-                    {/* <Link href="/products/body"> */}
-                        <div className="img two body"></div>
-                    {/* </Link> */}
+                    <Link href="/products/eyes" passHref>
+                        <a>
+                            <div className="img two eyes"></div>
+                        </a>
+                    </Link>
+                    <Link href="/products/body" passHref>
+                        <a>
+                            <div className="img two body"></div>
+                        </a>
+                    </Link>
                 </div>
                 <div className="row typeTwo">
-                    {/* <Link href="/products/hair"> */}
-                        <div className="img two hair"></div>
-                    {/* </Link> */}
-                    {/* <Link href="/products/sun"> */}
-                        <div className="img two sun"></div>
-                    {/* </Link> */}
+                    <Link href="/products/hair" passHref>
+                        <a>
+                            <div className="img two hair"></div>
+                        </a>
+                    </Link>
+                    <Link href="/products/sun" passHref>
+                        <a>
+                            <div className="img two sun"></div>
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
