00929dc42554d4efb25d76bf1a4797ba318c3771
Andriy_Step <andystep2008@gmail.com>
2024-07-03 19:44:37 +0300

    fixing katalog 6

--- src/app/components/categories/categories.jsx
+++ src/app/components/categories/categories.jsx
@@ -4,20 +4,50 @@ import Image from "next/image";
 import './categories.scss'
 
 const Categories = () => {
-    return(
+    return (
         <div className="categories">
             <h2>Категорії</h2>
             <div className="rows">
                 <div className="row typeOne">
-                    <div className="img one face"></div>
+                    <Image 
+                        className="img one face"
+                        src="/face.png"
+                        alt="Face"
+                        layout="fill"
+                        objectFit="cover"
+                    />
                 </div>
                 <div className="row typeTwo">
-                    <div className="img two eyes"></div>
-                    <div className="img two body"></div>
+                    <Image 
+                        className="img two eyes"
+                        src="/eyes.png"
+                        alt="Eyes"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                    <Image 
+                        className="img two body"
+                        src="/body.png"
+                        alt="Body"
+                        layout="fill"
+                        objectFit="cover"
+                    />
                 </div>
                 <div className="row typeTwo">
-                    <div className="img two hair"></div>
-                    <div className="img two sun"></div>
+                    <Image 
+                        className="img two hair"
+                        src="/hair.png"
+                        alt="Hair"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                    <Image 
+                        className="img two sun"
+                        src="/katalog/sun.png"
+                        alt="Sun"
+                        layout="fill"
+                        objectFit="cover"
+                    />
                 </div>
             </div>
         </div>
