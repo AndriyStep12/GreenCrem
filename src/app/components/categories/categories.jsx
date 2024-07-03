d637e7cddf86c84bdb48c12a578ce10cf8fc6047
Andriy_Step <andystep2008@gmail.com>
2024-07-03 20:02:15 +0300

    link categories

--- src/app/components/categories/categories.jsx
+++ src/app/components/categories/categories.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import './categories.scss'
+import Link from "next/link";
 
 const Categories = () => {
     return(
@@ -9,15 +10,25 @@ const Categories = () => {
             <h2>Категорії</h2>
             <div className="rows">
                 <div className="row typeOne">
-                    <div className="img one face"></div>
+                    <Link href={'/products/face'}>
+                        <div className="img one face"></div>
+                    </Link>
                 </div>
                 <div className="row typeTwo">
-                    <div className="img two eyes"></div>
-                    <div className="img two body"></div>
+                    <Link href={'/products/eyes'}>
+                        <div className="img two eyes"></div>
+                    </Link>
+                    <Link href={'/products/body'}>
+                        <div className="img two body"></div>
+                    </Link>
                 </div>
                 <div className="row typeTwo">
-                    <div className="img two hair"></div>
-                    <div className="img two sun"></div>
+                    <Link href={'/products/hair'}>
+                        <div className="img two hair"></div>
+                    </Link>
+                    <Link href={'/products/sun'}>
+                        <div className="img two sun"></div>
+                    </Link>
                 </div>
             </div>
         </div>
