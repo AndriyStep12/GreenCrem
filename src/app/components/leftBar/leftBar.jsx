5db4d5e89d893058f7303326e5d127aba41b251d
Andriy_Step <andystep2008@gmail.com>
2024-07-05 11:18:20 +0300

    update leftbar hiding

--- src/app/components/leftBar/leftBar.jsx
+++ src/app/components/leftBar/leftBar.jsx
@@ -1,26 +1,39 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import './leftBar.scss'
 import Linker from "./linker/linker";
 
 const LeftBar = () => {
-    return (
-        <div className="leftBar">
-            <h2 className="h2">Категорії</h2>
-            <ul>
-                <li><Linker name={`Догляд за обличчям`} path={'/products/face'} classN={'li'} /></li>
-                <li><Linker name={`Для зони навколо очей`} path={'/products/eyes'} classN={'li'} /></li>
-                <li><Linker name={`Догляд за тілом`} path={'/products/body'} classN={'li'} /></li>
-                <li><Linker name={`Догляд за волоссям`} path={'/products/hair'} classN={'li'} /></li>
-                <li><Linker name={`Сонцезахист`} path={'/products/sun'} classN={'li'} /></li>
-                <li><Linker name={`Всі продукти`} path={'/products/'} classN={'li'} /></li>
-            </ul>
-            <Linker name={`Головна сторінка`} path={'/'} classN={'h2'} />
-            <Linker name={`Контактна інформація`} path={'/'} classN={'h2'} />
-            <Linker name={`Про нас`} path={'/'} classN={'h2'} />
-        </div>
-    )
-}
+    const [isVisible, setIsVisible] = useState(false);
 
-export default LeftBar
+    const toggleLeftBar = () => {
+        setIsVisible(!isVisible);
+    };
+
+    return (
+        <>
+            <div className="centerLeftBar">
+                <button className="toggle-button" onClick={toggleLeftBar}>
+                    ☰
+                </button>
+            </div>
+            <div className={`leftBar ${isVisible ? 'visible' : 'hidden'}`}>
+                <h2 className="h2">Категорії</h2>
+                <ul>
+                    <li><Linker name={`Догляд за обличчям`} path={'/products/face'} classN={'li'} /></li>
+                    <li><Linker name={`Для зони навколо очей`} path={'/products/eyes'} classN={'li'} /></li>
+                    <li><Linker name={`Догляд за тілом`} path={'/products/body'} classN={'li'} /></li>
+                    <li><Linker name={`Догляд за волоссям`} path={'/products/hair'} classN={'li'} /></li>
+                    <li><Linker name={`Сонцезахист`} path={'/products/sun'} classN={'li'} /></li>
+                    <li><Linker name={`Всі продукти`} path={'/products/'} classN={'li'} /></li>
+                </ul>
+                <Linker name={`Головна сторінка`} path={'/'} classN={'h2'} />
+                <Linker name={`Контактна інформація`} path={'/'} classN={'h2'} />
+                <Linker name={`Про нас`} path={'/'} classN={'h2'} />
+            </div>
+        </>
+    );
+};
+
+export default LeftBar;
