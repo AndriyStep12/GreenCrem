b49fcd04d4037210be63ac66bfbd996e56019dd5
Andriy_Step <andystep2008@gmail.com>
2024-07-26 17:38:35 +0300

    fixing likes

--- src/functions/likes.jsx
+++ src/functions/likes.jsx
@@ -1,6 +1,7 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 
 const getWishlistItems = () => {
+  if (typeof window === 'undefined') return [];
   const items = localStorage.getItem('loved');
   return items ? JSON.parse(items) : [];
 };
